7865b5f5232abc596e85445aa95139e2e9de9342
son1cn <nelson.gaske@gmail.com>
2019-07-29 20:41:22 +0930

    add blink and rc_value.py scripts

---
+++ rc_value.py
@@ -0,0 +1,9 @@
+from gpiozero import LightSensor
+import time
+#setting up LightSensor on pin 4
+ldr = LightSensor(4)
+while True:
+	#just print value to help figure out setup rc value for algorithm
+	print(ldr.value)
+	#print every 0.1 secs
+	time.sleep(0.1)
