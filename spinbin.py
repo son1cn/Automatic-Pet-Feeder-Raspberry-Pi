24cde9aa6753e44227f8b767e166b768ed437743
son1cn <31121758+son1cn@users.noreply.github.com>
2020-04-16 09:24:42 +0930

    Update spinbin.py

--- spinbin.py
+++ spinbin.py
@@ -3,6 +3,7 @@
 #imports
 import RPi.GPIO as GPIO
 from gpiozero import LightSensor
+from gpiozero import Servo
 import time
 import signal
 import atexit
@@ -12,15 +13,15 @@ atexit.register(GPIO.cleanup)
 #pin init
 led_pin = 5
 
-servoPIN = 26
+#servoPIN = 26
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(servoPIN, GPIO.OUT, initial=False)
+#GPIO.setup(servoPIN, GPIO.OUT, initial=False)
 #GPIO.setmode(GPIO.BCM)
 GPIO.setup(led_pin,GPIO.OUT, initial=False)
+servo = Servo(26)
 
-
-p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
+#p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
 #p.start(0) # Initialization
 
 #setting up LightSensor on pin 4
@@ -40,25 +41,30 @@ try:
         #need to get away from light
         if ldr.value > 0.8:
             print("moving away from light")
-            p.start(0)
-            p.ChangeDutyCycle(7.2)
+            servo.value = 0.5
+ #           p.start(0)
+ #           p.ChangeDutyCycle(7.2)
             while ldr.value > 0.7:
                 time.sleep(0.05)
                 #print(ldr.value)
             print("final escape ldr ",ldr.value)
-            p.stop()
+#            p.stop()
+            servo.value = 0
         print("starting servo")
         print("starting ldr ", ldr.value)
-        p.start(0)
-        p.ChangeDutyCycle(6.9)
+#        p.start(0)
+#        p.ChangeDutyCycle(6.9)
+        servo.value = 0.5
         while ldr.value < 0.8:
             #print(ldr.value)
             time.sleep(0.05)
         print("final ldr ",ldr.value)
-        p.stop()
+#        p.stop()
+        servo.value = 0
         print("Motor stopped, LED Off")
         GPIO.output(led_pin, GPIO.LOW)
         time.sleep(5)
 except KeyboardInterrupt:
-  p.stop()
+#  p.stop()
+  servo.value = 0
   GPIO.cleanup()
