b30b6a63a1fae064a8b36b0d3b36326ed9ad7b54
son1cn <31121758+son1cn@users.noreply.github.com>
2020-04-29 18:36:07 +0930

    added output success log

--- spinbin.py
+++ spinbin.py
@@ -3,7 +3,7 @@
 #imports
 from gpiozero import LightSensor
 import pigpio
-import time
+import time, csv, datetime
 
 #LED is connected to pin 5
 led_pin = 5
@@ -19,18 +19,22 @@ pi = pigpio.pi()
 #All LDR values further down need to be calibrated for your setup
 ldr = LightSensor(4)
 
+def write_log(output):
+    try:
+        with open('feeder_log', 'w') as outfile:
+            outwrite = csv.writer(outfile)
+            outwrite.writerow(datetime.now.strftime("%d/%m/%Y %H:%M:%S"),output)
 try:
     while True:
         print("LED turn on")
         pi.write(led_pin, 1)
-#        time.sleep(1)
         print(ldr.value)
-	#if something is wrong with LDR it returns 0.0
-	if ldr.value == 0.0:
-	    print("you shouldn't see this")
-	    ldr.close()
-	    time.sleep(1)
-	    ldr = LightSensor(4)
+	    #if something is wrong with LDR it returns 0.0
+        if ldr.value == 0.0:
+            print("you shouldn't see this")
+            ldr.close()
+            time.sleep(1)
+            ldr = LightSensor(4)
         #If LDR has higher than 0.8 (for my setup) move a bit before collecting LDR input
         if ldr.value > 0.8:
             print("moving away from light")
@@ -40,8 +44,8 @@ try:
                 time.sleep(0.1)
                 pi.set_servo_pulsewidth(servoPIN, STOP)
                 time.sleep(0.5)
-		print(ldr.value)
-            print("final escape ldr ",ldr.value)
+        print(ldr.value)
+        print("final escape ldr ",ldr.value)
         print("starting servo")
         print("starting ldr ", ldr.value)
         #while LDR is <0.8 continue to move in 0.1s increments of slowest servo speed
@@ -55,7 +59,7 @@ try:
         #turn off LED
         print("Motor stopped, LED Off")
         pi.write(led_pin, 0)
- #       time.sleep(5)
-	break
+        write_log("Fed")
+        break
 except KeyboardInterrupt:
     pi.stop()
